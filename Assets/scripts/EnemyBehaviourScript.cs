9b768768f64d936feb790e46cad9483c2b490223
bfuchs2 <bernardtfuchs@gmail.com>
2014-06-11 12:12:19 -0700

    first commit

--- Assets/scripts/EnemyBehaviourScript.cs
+++ Assets/scripts/EnemyBehaviourScript.cs
@@ -8,12 +8,4 @@ public class EnemyBehaviourScript : MonoBehaviour {
 		transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	IEnumerator walk(){
-		yield return new WaitForSeconds(0.1f);
-	}
 }
