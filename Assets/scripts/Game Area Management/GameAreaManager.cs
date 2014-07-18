7a660a2ca342f4b156f8d8e1f47d5c53481b7c8b
bfuchs2 <bernardtfuchs@gmail.com>
2014-07-18 12:12:59 -0700

    removed duplicate assets directory

--- Assets/scripts/Game Area Management/GameAreaManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameAreaManager : MonoBehaviour {
-
-	void OnTriggerExit(Collider other){
-		if(other.tag != "Enemy") Destroy(other.gameObject);
-	}
-}
--- assets/scripts/GameAreaManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameAreaManager : MonoBehaviour {
-
-	void OnTriggerExit(Collider other){
-		Destroy(other.gameObject);
-	}
-}
